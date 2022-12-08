cc5e2a70f7e5f61b9e3227da3d126e853dc25e21
Danya Zverev <i_am_zver@yahoo.com>
2022-12-08 20:57:34 +0600

    second_part

--- Problem1007/Main.java
+++ Problem1007/Main.java
@@ -1,2 +1,86 @@
-package Problem1007;public class Main {
+package Problem1007;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static List<Student> students = new ArrayList<Student>();
+    static StringBuilder readInformation = new StringBuilder();
+    static int altynCount = 0;
+
+    public static void main(String[] args) {
+        inputData();
+        addStudents();
+        printStudents();
+    }
+
+    static void inputData(){
+        try{
+            FileInputStream fis = new FileInputStream("src/Problem1007/toRead.txt");
+            byte[] bytesData = fis.readAllBytes();
+
+            for (byte b : bytesData) {
+                readInformation.append((char) b);
+            }
+            fis.close();
+        } catch (IOException e){
+            System.out.println(e);
+        }
+    }
+
+    static void addStudents() {
+        String[] allStudentsData = readInformation.toString().split("\r\n");
+
+        for (String student : allStudentsData) {
+            String[] eachStDataArr = student.split("( )");
+
+            String stName = eachStDataArr[0] + " " + eachStDataArr[1];
+            int[] grades = new int[4];
+            boolean isAltyn = (eachStDataArr[6].equalsIgnoreCase("yes".strip()));
+            if (isAltyn) altynCount++;
+
+            for (int j = 0; j < 4; j++) {
+                grades[j] = Integer.parseInt(eachStDataArr[j + 2]);
+            }
+
+            students.add(new Student(stName, grades, isAltyn));
+        }
+
+        Collections.sort(students);
+        for (Student s : students) System.out.println(s);
+        System.out.println(allStudentsData.length + " students were successfully added!");
+    }
+
+    static void printStudents() {
+        System.out.print("Count of grants: ");
+        int grantCount = scanner.nextInt();
+
+        ListIterator<Student> stItr = students.listIterator();
+
+        int i = 0;
+        while (stItr.hasNext() && (grantCount - altynCount) > i){
+            Student st = stItr.next();
+            System.out.println(st);
+            if (st.getIsAltyn()) altynCount--;
+            stItr.previous();
+            stItr.remove();
+            i++;
+        }
+
+        if (altynCount > 0) {
+            findAltyn();
+        }
+    }
+
+    static void findAltyn() {
+        for (Student s : students) {
+            if (s.getIsAltyn()) {
+                System.out.println(s);
+            }
+        }
+    }
+
+
 }
--- Problem1007/Student.java
+++ Problem1007/Student.java
@@ -1,2 +1,60 @@
-package Problem1007;public class Student {
+package Problem1007;
+
+public class Student implements Comparable<Student>{
+    private int ID;
+    private String name;
+    private int[] grades;
+    private boolean isAltyn;
+    private int sumPoints;
+
+    public Student(String name, int[] grades, boolean isAltyn) {
+        this.ID = (int) (Math.random() * 1000 - 1);
+        this.name = name;
+        this.grades = grades;
+        this.isAltyn = isAltyn;
+        this.sumPoints = gradeSum();
+    }
+
+    private int gradeSum(){
+        int sum = 0;
+        for (int i : grades) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int[] getGrades() {
+        return grades;
+    }
+
+    public boolean getIsAltyn() {
+        return isAltyn;
+    }
+
+    public int getSumPoints() {
+        return sumPoints;
+    }
+
+    @Override
+    public int compareTo(Student s) {
+        return Integer.compare(sumPoints * (-1), s.getSumPoints() * (-1));
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "ID=" + ID +
+                ", name=" + name +
+                ", sumPoints=" + sumPoints +
+                ", getIsAltyn = " + isAltyn +
+                '}';
+    }
 }
